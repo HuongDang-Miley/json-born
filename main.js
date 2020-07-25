557eb2d0cdc9f779deaaa78bf6299d75a4a14e17
Huong Dang <miley.dawn@codeimmersives.com>
2020-07-25 16:05:14 -0400

    HW

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
+// read file
+const fs = require('fs')
+const file = fs.readFileSync('../students.json', 'utf8')
+// convert to object
+const obj = JSON.parse(file)
 
 
+const getStudentByIndex = function(index) {
+  return obj.students[index]
+}
 
 
+const getStudentByName = function(name) {
+  for (const student of obj.students ) {
+    if (student.name === name) {
+      return student
+    }
+  }
+  return -1
+}
 
+
+const graduateStudent = function(name) {
+  for (const student of obj.students) {
+    if (student.name === name) {
+      student.term ++
+    }
+  }
+  const updateFile = JSON.stringify(obj, null, 2)
+  fs.writeFileSync('../students.json', updateFile)
+}
 
 
 if (typeof getStudentByIndex === 'undefined') {
